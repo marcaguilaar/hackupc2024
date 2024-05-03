f37f1f3c3b20c404da4e984603457f3ffeda0c5a
Oriol Ramos <oramos@oramos-xps>
2024-05-04 00:38:52 +0200

    screen pruebas

--- hackupc/App.js
+++ hackupc/App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+//Pantallas
+import Pruebas from './screens/Pruebas';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up HackUPC.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Pruebas" component={Pruebas} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
